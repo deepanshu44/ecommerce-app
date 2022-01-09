74291954e20c045728fc4f195fba590fe11eaa25
deepanshu <deepanshuyadav.04@gmail.com>
2022-01-09 23:04:09 +0530

    modified

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,8 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
 const isProduction = process.env.NODE_ENV == 'production';
-
-
 const stylesHandler = MiniCssExtractPlugin.loader;
-
-
 
 const config = {
     entry: './src/index.ts',
@@ -27,8 +20,6 @@ const config = {
 
         new MiniCssExtractPlugin(),
 
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
     module: {
         rules: [
@@ -46,8 +37,6 @@ const config = {
                 type: 'asset',
             },
 
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
         ],
     },
     resolve: {
@@ -58,7 +47,6 @@ const config = {
 module.exports = () => {
     if (isProduction) {
         config.mode = 'production';
-        
         
     } else {
         config.mode = 'development';
